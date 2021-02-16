5d01f6a08393d402e4e83dbc341deff52eb450eb
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-02-16 11:44:51 +0100

    Ik heb het kapot gemaakt
    What a surprise

--- script/app.js
+++ script/app.js
@@ -5,16 +5,71 @@ const continents = {
     europe: 'Europe',
     oceania: 'Oceania',
 }
-    endpoint = 'https://restcountries.eu/rest/v2'
+    endpoint = 'https://restcountries.eu/rest/v2',
+    LOCAL_STORAGE_KEY = 'countries';
 
 let countryHolder, regioRadioButtons;
+
+const saveCountry = (alpha2Code, add) => {
+    const savedCountries = localStorage.LOCAL_STORAGE_KEY; //Hier een key gebruiken, maakt het dynamisch voor herbruikbaarheid
+    const selectedRegion = document.querySelector('.js-region-radio:checked').value;
+    if(!savedCountries && add){
+        const initialData = {
+            [selectedRegion]: {[alpha2Code]: true},
+        };
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(initialData));
+        return; //stap uit deze functie, het is afgehandeld
+    } else {
+        if(add) {
+            const savedData = JSON.parse(savedCountries);
+            if (savedData[selectedRegion]) {
+                savedData[selectedRegion][alpha2Code] = true; //Key toevoegen aan bestaand (region) object
+            
+            } else {
+            savedData[selectedRegion] = { [alpha2Code]: true}; //Nieuwe object toekennen aan (nieuwe) region
+
+        }
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedData));
+    } else {
+        savedData = JSON.parse(savedCountries);
+        delete savedData[selectedRegion][alpha2Code];
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedData));
+
+        }
+    }
+
+};
+
+const listenToSavedCountries = () => {
+    const countries = document.querySelectorAll('.js-country-input');
+    console.log({countries});
+    for (const country of countries) {
+        country.addEventListener('change', function(){
+            saveCountry(this.id, this.checked);
+        });
+        
+    }
+
+};
+
+const searchLocalStorageFor = (alpha2Code) => {
+    const localData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    const selectedRegion = document.querySelector('.js-region-radio:checked').value;
+
+    if(!localData[selectedRegion][alpha2Code]){
+        return 'checked';
+    }else {
+        return;
+    }
+}
 
 const renderCountries = (countries) => {
     let countriesHTML ='';
 
     for (const {name, alpha2Code, flag, nativeName} of countries) {
         countriesHTML+= ` <section class="c-country">
-        <input checked class="c-country__hidden-input o-hide-accessible" type="checkbox" name="country" id="${alpha2Code}">
+        <input checked class="c-country__hidden-input o-hide-accessible js-country-input" type="checkbox" name="country" id="${alpha2Code}" ${searchLocalStorageFor(alpha2Code)}
+        >
         <label class="c-country__label" for="${alpha2Code}">
             <div class="c-country__flag-holder">
         <img class="c-country__flag" src="${flag}" alt="The Flag of ${name}">
@@ -26,16 +81,20 @@ const renderCountries = (countries) => {
         </label>
     </section> `;
     }
+    
 
     countryHolder.innerHTML = countriesHTML;
+    listenToSavedCountries();
 
-    console.log(countries);
+
+    // console.log(countries);
 }
 
 const getCountries = async (continent) => {
     const data = await get(`${endpoint}/region/${continent}`)
-
+    console.log(data);
     renderCountries(data);
+    
 }
 
 const enableNavigation = () =>{
